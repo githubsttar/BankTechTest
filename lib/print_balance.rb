4656cf15edde7c3b0a9fd0abf4eba3799df61ad7
Ijeoma Nelson <ijeoma.nelson1@gmail.com>
2019-01-16 18:30:07 +0000

    passed one failing test for print_balance

--- lib/print_balance.rb
+++ lib/print_balance.rb
@@ -4,5 +4,46 @@ class PrintBalance
   end
 
   def print_balance(display_transaction)
+    @display_transaction = display_transaction
+    puts title + print
   end
+
+  private
+
+  def title
+    date = "date".ljust(8)
+    credit = "credit".ljust(8)
+    debit = "debit".ljust(8)
+    balance = "balance\n"
+    spacer = " || "
+
+    date + spacer + credit + spacer + debit + spacer + balance
+  end
+
+  def account_balance
+    @transactions = []
+    @display_transaction.transactions.each do |tr, balance|
+      @transactions << "#{date(tr)} || #{make_deposit(tr)} || #{make_withdrawal(tr)} || #{sprintf('%.2f', balance)}"
+    end
+  end
+
+  def print
+    account_balance
+    @transactions.map { |tr| "#{tr}" }.join("\n")
+  end
+
+  def date(tr)
+    tr.date.strftime("%d/%m/%Y").ljust(15)
+  end
+
+  def make_deposit(tr)
+    str = ""
+    tr.type == :deposit ? "#{sprintf('%.2f', tr.amount)} " : str.ljust(8)
+  end
+
+  def make_withdrawal(tr)
+    str = ""
+    tr.type == :withdrawal ? "#{sprintf('%.2f', -tr.amount)} " : str.ljust(8)
+  end
+
 end
--- spec/print_balance_spec.rb
+++ spec/print_balance_spec.rb
@@ -8,15 +8,15 @@ describe PrintBalance do
     Timecop.freeze(@withdrawal_time)
   end
 
-  let(:deposit) {double(:transaction, amount: 100, date: @deposit_time, type: :deposit)}
-  let(:withdrawal) {double(:transaction, amount: -50, date: @withdrawal_time, type: :withdrawal)}
+  let(:deposit) {double(:transaction, amount: 1000, date: @deposit_time, type: :deposit)}
+  let(:withdrawal) {double(:transaction, amount: -2000, date: @withdrawal_time, type: :withdrawal)}
   let(:display_transaction) {instance_double("display_transaction")}
   subject(:account_balance) {described_class.new}
 
   describe '#print_balance' do
     it 'prints out the statement with header' do
-      allow(display_transaction).to receive(:transactions){{deposit => 100, withdrawal => 50}}
-      print_out = "date       || credit   || debit    || balance\n16/01/2019 || 100.00  ||          || 50.00\n16/01/2019 ||          || 200.00  || 100.00"
+      allow(display_transaction).to receive(:transactions){{deposit => 1000, withdrawal => 800}}
+      print_out = "date       || credit   || debit    || balance\n16/01/2019 || 1000.00  ||          || 1000.00\n16/01/2019 ||          || 2000.00  || 800.00"
       expect(STDOUT).to receive(:puts).with(print_out)
       account_balance.print_balance(display_transaction)
     end
