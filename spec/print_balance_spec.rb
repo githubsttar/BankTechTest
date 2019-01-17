ea477823b6937b4aa0741e64517bab7d590c4066
Ijeoma Nelson <ijeoma.nelson1@gmail.com>
2019-01-17 09:37:50 +0000

    deleted a call to pry from print_balance

--- spec/print_balance_spec.rb
+++ spec/print_balance_spec.rb
@@ -18,7 +18,6 @@ describe PrintBalance do
     it 'prints out the statement with header' do
       allow(balance_log).to receive(:transactions){{deposit => 1000, withdrawal => 800}}
       print_out = "date       || credit   || debit    || balance\n16/01/2019 || 1000.00  ||          || 1000.00\n16/01/2019 ||          || 2000.00  || 800.00"
-      # binding.pry
       expect(STDOUT).to receive(:puts).with(print_out)
       account_balance.print_balance(balance_log)
     end
